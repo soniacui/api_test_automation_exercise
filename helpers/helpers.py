726498a228e3a2b8f2b876eecdda432d196d0a54
soniacui <soniacuisc@gmail.com>
2026-07-14 15:47:32 -0400

    created helpers to consolidate json checks + refactored test file names

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -15,3 +15,9 @@ def load_test_expected(endpoint: str, test_name: str) -> dict:
     file_name = f"{test_name}_expected.json"
     with (FIXTURES_DIR / "expected" / endpoint / file_name).open("r", encoding="utf-8") as f:
         return json.load(f)
+
+
+def assert_json_response(response, expected_status_code, expected_body):
+    assert response.status_code == expected_status_code
+    assert response.headers["content-type"].startswith("application/json")
+    assert response.json() == expected_body
